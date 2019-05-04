24f1b139a85192b28b620626aa81ea64913733a5
Yusuf Kerem Ozaydin <ykerem99@gmail.com>
2019-05-04 14:48:39 -0400

    Added firebase login/signup

--- JS/SignUpPage.js
+++ JS/SignUpPage.js
@@ -1,22 +1,25 @@
-const db = firebase.firestore();
-const auth = firebase.auth();
-
-db.collection("users")
-  .get()
-  .then(snapshot => snapshot.docs.forEach(doc => console.log(doc.data())));
-
 function onSubmit() {
   email = document.getElementById("inputEmail").value;
   password = document.getElementById("inputPassword").value;
+  state = document.getElementById("inputState").value;
   console.log(email, password);
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
+  auth
+    .createUserWithEmailAndPassword(email, password)
     .then(user => {
       alert("Successfully signed in.");
       //user unique ID
       console.log(user.user.uid);
-      //Redirect to home page.
+      //Write data
+      db.collection("users")
+        .doc(user.user.uid)
+        .set({
+          email: email,
+          state: state
+        })
+        .then(() => {
+          console.log("Successful");
+        })
+        .catch(error => alert("Error writing document: ", error));
     })
     .catch(function(error) {
       // Handle Errors here.
